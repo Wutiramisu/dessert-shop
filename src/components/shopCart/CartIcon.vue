13dfaa8e7060c9a079edb2867dad1bf935c89f25
chung yu wu <wutiramisu9205@gmail.com>
2021-02-26 01:49:35 +0800

    優化圖示顯示方式 碰到footer不重疊

--- src/components/shopCart/CartIcon.vue
+++ src/components/shopCart/CartIcon.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-link to="/cart" v-if="!hideIcon && !isEmptyCart" class="cart__wrapper" :class="{ 'num-change': isChange }">
+  <router-link
+    to="/cart"
+    v-if="!hideIcon && !isEmptyCart"
+    class="cart__wrapper"
+    :class="{ 'num-change': isChange }"
+    :style="{ bottom: iconBottom + 'px' }"
+  >
     <i class="fas fa-shopping-cart cart__icon"></i>
     <div class="cart__num">{{ cartNum }}</div>
   </router-link>
@@ -10,8 +16,26 @@ export default {
   data () {
     return {
       hideIcon: false,
-      isChange: false
+      isChange: false,
+      iconBottom: 100,
+      footerHeigh: 150,
+      clientHeight: null,
+      scrollTop: null,
+      innerHeight: null
     };
+  },
+  created () {
+    window.addEventListener('scroll', this.scrollEvent);
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.scrollEvent);
+  },
+  methods: {
+    scrollEvent () {
+      this.clientHeight = document.body.clientHeight;
+      this.scrollTop = document.scrollingElement.scrollTop;
+      this.innerHeight = window.innerHeight;
+    }
   },
   computed: {
     cartNum () {
@@ -34,6 +58,14 @@ export default {
       setTimeout(() => {
         this.isChange = !this.isChange;
       }, 500);
+    },
+    scrollTop () {
+      const bottomDistance = this.clientHeight - this.scrollTop - this.innerHeight;
+      if (bottomDistance <= this.footerHeigh) {
+        this.iconBottom = this.footerHeigh - bottomDistance + 100;
+      } else if (this.iconBottom > 100) {
+        this.iconBottom = 100;
+      }
     }
   }
 };
@@ -48,12 +80,11 @@ export default {
     height: 7rem;
     position: fixed;
     right: 3.5rem;
-    bottom: 25rem;
     z-index: 100;
     border-radius: 50%;
     background-color: rgba(178, 132, 81, .7);
     color: var(--color-secondary);
-    transition: .5s;
+    transition: transform .5s, background-color .5s;
 
     &:hover {
       background-color: rgba(198, 153, 99, 1);
@@ -61,7 +92,6 @@ export default {
   }
 
   &__icon {
-    // font-size: 2rem;
     position: absolute;
     top: 65%;
     left: 48%;
@@ -70,7 +100,6 @@ export default {
 
   &__num {
     font-family: 'Montserrat';
-    // font-size: 2rem;
     font-weight: 700;
     position: absolute;
     top: 30%;
@@ -82,7 +111,5 @@ export default {
 .num-change {
   background: rgb(198, 153, 99) !important;
   transform: rotateY(90deg);
-//   bottom: 15rem;
-//   opacity: 0;
 }
 </style>
